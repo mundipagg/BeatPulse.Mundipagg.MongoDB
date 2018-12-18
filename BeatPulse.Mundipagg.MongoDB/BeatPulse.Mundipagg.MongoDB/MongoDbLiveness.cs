1f9976f60fece13cd8288293e22c5c5d7b7a439b
Lucas Nobre <lucasnobredev@outlook.com>
2018-12-18 01:46:16 -0200

    implementação já existente no id-api

--- BeatPulse.Mundipagg.MongoDB/BeatPulse.Mundipagg.MongoDB/MongoDbLiveness.cs
+++ BeatPulse.Mundipagg.MongoDB/BeatPulse.Mundipagg.MongoDB/MongoDbLiveness.cs
@@ -1,4 +1,6 @@
 ﻿using BeatPulse.Core;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,9 +11,28 @@ namespace BeatPulse.Mundipagg.MongoDB
 {
     public class MongoDbLiveness : IBeatPulseLiveness
     {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbLiveness(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetSection("MongoDbConnectionString").Value;
+            var client = new MongoClient(connectionString);
+
+            _database = client.GetDatabase(new MongoUrl(connectionString).DatabaseName);
+        }
+
         public async Task<LivenessResult> IsHealthy(LivenessExecutionContext context, CancellationToken cancellationToken = default(CancellationToken))
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _database.ListCollectionNamesAsync();
+
+                return LivenessResult.Healthy("The service is operating");
+            }
+            catch (Exception ex)
+            {
+                return LivenessResult.UnHealthy(ex.Message);
+            }
         }
     }
 }
